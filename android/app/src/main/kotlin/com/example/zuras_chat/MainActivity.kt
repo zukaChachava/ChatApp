b5a71a54fb0a4d55610b912b872aa247ed492558
zuka <zurabi.chachava.1@btu.edu.ge>
2022-07-25 11:32:54 +0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/zuras_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.zuras_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (ctx, index) {
+            return Container(
+                padding: const EdgeInsets.all(8), child: Text('This works!'));
+          }),
+    );
+  }
+}
