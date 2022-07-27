6595ebebcee741b461bd812303b94bfbded13a70
zuka <zurabi.chachava.1@btu.edu.ge>
2022-07-27 17:05:13 +0400

    messaging works

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -21,45 +21,63 @@ class MessageBubble extends StatelessWidget {
     return Row(
       mainAxisAlignment: isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        FutureBuilder<dynamic>(
-          future:
-              FirebaseFirestore.instance.collection('users').doc(userId).get(),
-          builder: ((context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Text('Loading...');
-            }
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (!isMe)
+              Container(
+                margin: const EdgeInsets.only(left: 20),
+                child: FutureBuilder<dynamic>(
+                  future: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(userId)
+                      .get(),
+                  builder: ((context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Text('Loading...');
+                    }
 
-            print(json.decode(snapshot.data));
+                    String? username;
 
-            return Text(
-              snapshot.data[userId]['username'],
-              style: const TextStyle(fontWeight: FontWeight.bold),
-            );
-          }),
-        ),
-        Container(
-          decoration: BoxDecoration(
-            color: isMe
-                ? Theme.of(context).colorScheme.secondary
-                : Theme.of(context).colorScheme.primary,
-            borderRadius: BorderRadius.only(
-                topLeft: const Radius.circular(12),
-                topRight: const Radius.circular(12),
-                bottomLeft:
-                    isMe ? const Radius.circular(12) : const Radius.circular(0),
-                bottomRight: isMe
-                    ? const Radius.circular(0)
-                    : const Radius.circular(12)),
-          ),
-          width: 140,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 18),
-          child: Text(
-            message,
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onPrimary,
+                    try {
+                      username = snapshot.data['username'];
+                    } catch (err) {
+                      username = "Unknown";
+                    }
+
+                    return Text(
+                      username!,
+                      style: const TextStyle(fontWeight: FontWeight.bold),
+                    );
+                  }),
+                ),
+              ),
+            Container(
+              decoration: BoxDecoration(
+                color: isMe
+                    ? Theme.of(context).colorScheme.secondary
+                    : Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.only(
+                    topLeft: const Radius.circular(12),
+                    topRight: const Radius.circular(12),
+                    bottomLeft: isMe
+                        ? const Radius.circular(12)
+                        : const Radius.circular(0),
+                    bottomRight: isMe
+                        ? const Radius.circular(0)
+                        : const Radius.circular(12)),
+              ),
+              width: 140,
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 18),
+              child: Text(
+                message,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
             ),
-          ),
+          ],
         )
       ],
     );
